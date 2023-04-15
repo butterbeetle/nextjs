aad79672137a964e63d28bee47d0f2a182a82e33
butterbeetle <butterbeetle@naver.com>
2023-04-15 20:13:36 +0900

    프로그래밍 방식으로 네비게이팅 ex) Form 제출

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -3,9 +3,17 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+  const loadProjectHandler = () => {
+    // load data...
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  };
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
