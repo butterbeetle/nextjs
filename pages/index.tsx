e1f2366659b36fac876af1d239fdc7e7d3f826aa
butterbeetle <butterbeetle@naver.com>
2023-04-17 22:03:43 +0900

    Section05 시작

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,13 @@ function HomePage() {
       <h1>Section Select</h1>
       <ul>
         <li>
-          <Link href="/section03">Section03</Link>
+          <Link href="/section03">Section 03</Link>
         </li>
         <li>
-          <Link href="/section04">Section04</Link>
+          <Link href="/section04">Section 04</Link>
+        </li>
+        <li>
+          <Link href="/section05">Section 05</Link>
         </li>
       </ul>
     </div>
